d34e12205dc0629668f12de568d149b76180c74c
Arshad Anas <arshadanas900+github@gmail.com>
2024-10-19 19:33:47 -0400

    move Lift to its own class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Deposit.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static com.arcrobotics.ftclib.hardware.motors.Motor.GoBILDA.RPM_312;
 import static com.arcrobotics.ftclib.hardware.motors.Motor.ZeroPowerBehavior.FLOAT;
 import static org.firstinspires.ftc.teamcode.opmodes.MainAuton.mTelemetry;
 import static org.firstinspires.ftc.teamcode.subsystems.Deposit.State.ABOVE_HIGH_RUNG;
@@ -19,7 +18,6 @@ import static org.firstinspires.ftc.teamcode.subsystems.Intake.Sample.BLUE;
 import static org.firstinspires.ftc.teamcode.subsystems.Intake.Sample.NEUTRAL;
 import static org.firstinspires.ftc.teamcode.subsystems.Intake.Sample.NONE;
 import static org.firstinspires.ftc.teamcode.subsystems.Intake.Sample.RED;
-import static org.firstinspires.ftc.teamcode.subsystems.Robot.maxVoltage;
 import static org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot.getAxonServo;
 import static org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot.getGoBildaServo;
 import static org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot.getReversedServo;
@@ -28,12 +26,9 @@ import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.control.controllers.PIDController;
 import org.firstinspires.ftc.teamcode.control.gainmatrices.HSV;
-import org.firstinspires.ftc.teamcode.control.gainmatrices.PIDGains;
 import org.firstinspires.ftc.teamcode.subsystems.utilities.SimpleServoPivot;
 import org.firstinspires.ftc.teamcode.subsystems.utilities.sensors.ColorSensor;
 
@@ -456,96 +451,4 @@ public final class Deposit {
         mTelemetry.addData("Deposit", (sample == NONE ? "empty" : "contains a " + sample.name() + " sample"));
     }
 
-    @Config
-    public static final class Lift {
-
-        public static PIDGains pidGains = new PIDGains(
-                0.3,
-                0.2,
-                0,
-                0.2
-        );
-
-        public static double
-                kG = 0.15,
-                INCHES_PER_TICK = 0.0088581424,
-                HEIGHT_RETRACTED_THRESHOLD = 0.5;
-
-        // Motors and variables to manage their readings:
-        private final MotorEx[] motors;
-        private final Motor.Encoder encoder;
-        private org.firstinspires.ftc.teamcode.control.motion.State currentState, targetState;
-        private final PIDController controller = new PIDController();
-
-        private double manualLiftPower;
-        private int encoderOffset;
-
-        // Battery voltage sensor and variable to track its readings:
-        private final VoltageSensor batteryVoltageSensor;
-
-        private Lift(HardwareMap hardwareMap) {
-            this.batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
-            this.motors = new MotorEx[]{
-                    new MotorEx(hardwareMap, "lift right", RPM_312),
-                    new MotorEx(hardwareMap, "lift left", RPM_312)
-            };
-            motors[1].setInverted(true);
-            for (MotorEx motor : motors) motor.setZeroPowerBehavior(FLOAT);
-
-            encoder = new MotorEx(hardwareMap, "right back", RPM_312).encoder;
-
-            reset();
-        }
-
-        public void reset() {
-            controller.reset();
-            encoderOffset = encoder.getPosition();
-            targetState = currentState = new org.firstinspires.ftc.teamcode.control.motion.State();
-        }
-
-        private boolean isExtended() {
-            return currentState.x > HEIGHT_RETRACTED_THRESHOLD;
-        }
-
-        public void setTargetPosition(double inches) {
-            this.targetState = new org.firstinspires.ftc.teamcode.control.motion.State(inches);
-        }
-
-        public void setLiftPower(double manualLiftPower) {
-            this.manualLiftPower = manualLiftPower;
-        }
-
-        void readSensors() {
-            currentState = new org.firstinspires.ftc.teamcode.control.motion.State(INCHES_PER_TICK * (encoder.getPosition() - encoderOffset));
-            controller.setGains(pidGains);
-        }
-
-        private void run(boolean intakeClear) {
-
-            if (manualLiftPower != 0) targetState = currentState; // replace PID target with current state if using manual control
-
-            controller.setTarget(intakeClear ? targetState : new org.firstinspires.ftc.teamcode.control.motion.State(0)); // set PID target to 0 (retract) if intake isn't yet out of the way
-
-            boolean retracted = !isExtended();
-
-            double voltageScalar = maxVoltage / batteryVoltageSensor.getVoltage();
-
-            double gravityFeedforward = retracted ? 0 : kG * voltageScalar;
-
-            double output = gravityFeedforward + (
-                    manualLiftPower != 0 ?                              // if manual input is being used:
-                            manualLiftPower * voltageScalar :               // control with manual power (and voltage compensate)
-                            controller.calculate(currentState)  // control with PID output
-            );
-
-            for (MotorEx motor : motors) motor.set(output);
-        }
-
-        void printNumericalTelemetry() {
-            mTelemetry.addData("Current position (in)", currentState.x);
-            mTelemetry.addData("Target position (in)", targetState.x);
-            mTelemetry.addLine();
-            mTelemetry.addData("Lift error derivative (in/s)", controller.getFilteredErrorDerivative());
-        }
-    }
 }
