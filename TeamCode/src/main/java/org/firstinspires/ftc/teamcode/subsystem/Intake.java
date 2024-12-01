762abef493fd2a1b1995447e3ac6a203ec186b7c
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-01 16:19:28 -0500

    allow re-extension if no sample obtained (retracted with button)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -278,6 +278,10 @@ public final class Intake {
     public void setExtended(boolean extend) {
         switch (state) {
 
+            case EXTENDO_RETRACTING:
+            case BUCKET_RETRACTING:
+            case BUCKET_SETTLING:
+                if (hasSample()) break;
             case RETRACTED:
 
                 if (extend) {
