33cd5895700389a8e6ab8d3959d7c4e3ae4eb824
Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>
2024-12-20 18:06:17 -0500

    installed meepmeep

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonVars.java
@@ -0,0 +1,37 @@
+package com.example.meepmeeptesting;
+
+import static java.lang.Math.PI;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+import java.util.PrimitiveIterator;
+
+public final class AutonVars {
+
+    public static double
+            SIZE_WINDOW = 720,
+            LENGTH_ROBOT = 17.3984665354,
+            WIDTH_ROBOT = 16.4220472441,
+            SIZE_HALF_FIELD = 70.5,
+            SIZE_TILE = 23.625,
+            X_START_LEFT = SIZE_TILE * -1.5,
+            X_START_RIGHT = SIZE_TILE * 0.5,
+            Y_START = -SIZE_HALF_FIELD + LENGTH_ROBOT * 0.5;
+
+    enum ParkingLocation {
+        CORNER,
+        OUTER,
+        TOUCHING_RUNG;
+
+        public static final ParkingLocation[] locations = values();
+
+        public ParkingLocation plus(int i) {
+            int x = ordinal() + 1, max = locations.length;
+            return locations[(x % max + max) % max];
+        }
+    }
+
+    public static EditablePose
+            startPose = new EditablePose(X_START_RIGHT, Y_START, 0.5 * PI)
+            ;
+}
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/EditablePose.java
@@ -0,0 +1,26 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+public final class EditablePose {
+
+    public double x, y, heading;
+
+    public EditablePose(double x, double y, double heading) {
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+
+    public EditablePose(Pose2d pose) {
+        this(pose.position.x, pose.position.y, pose.heading.log());
+    }
+
+    public EditablePose clone() {
+        return new EditablePose(x, y, heading);
+    }
+
+    public Pose2d toPose2d() {
+        return new Pose2d(x, y, heading);
+    }
+}
