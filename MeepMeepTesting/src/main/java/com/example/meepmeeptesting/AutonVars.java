cda737944244fc20f75732884da7a910b6eef512
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-25 14:13:34 -0500

    2 sample auto in meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonVars.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutonVars.java
@@ -1,18 +1,8 @@
 package com.example.meepmeeptesting;
 
-import static java.lang.Math.PI;
+import static java.lang.Math.toRadians;
 
 public final class AutonVars {
-
-    public static double
-            SIZE_WINDOW = 720,
-            LENGTH_ROBOT = 17.30327,
-            WIDTH_ROBOT = 16.42126,
-            SIZE_HALF_FIELD = 70.5,
-            SIZE_TILE = 23.625,
-            X_START_LEFT = SIZE_TILE * -1.5,
-            X_START_RIGHT = SIZE_TILE * 0.5,
-            Y_START = -SIZE_HALF_FIELD + LENGTH_ROBOT * 0.5;
 
     enum ParkingLocation {
         CORNER,
@@ -27,6 +17,28 @@ public final class AutonVars {
         }
     }
 
+    public static double
+            SIZE_WINDOW = 720,
+            LENGTH_ROBOT = 17.30327,
+            WIDTH_ROBOT = 16.42126,
+            SIZE_HALF_FIELD = 70.5,
+            SIZE_TILE = 23.625,
+            X_START_LEFT = SIZE_TILE * -1.5,
+            X_START_RIGHT = SIZE_TILE * 0.5,
+            Y_START = -SIZE_HALF_FIELD + LENGTH_ROBOT * 0.5,
+            WAIT_APPROACH_BASKET = 0,
+            WAIT_SCORE_BASKET = 1.5,
+            WAIT_POST_INTAKING = 0.5,
+            WAIT_MAX_INTAKING = 3,
+            EXTEND_SAMPLE_1 = 300,
+            EXTEND_SAMPLE_2 = 300,
+            EXTEND_SAMPLE_3 = 300;
+
     public static EditablePose
-            startPose = new EditablePose(X_START_RIGHT, Y_START, 0.5 * PI);
+            sample1Floor = new EditablePose(-48, -27.75, toRadians(90)),
+            basket = new EditablePose(-56, -56, toRadians(45)),
+            sample1 = new EditablePose(-50, -48, toRadians(84.36)),
+            sample2 = new EditablePose(-54, -45, toRadians(105)),
+            sample3 = new EditablePose(-48, -49, toRadians(120)),
+            parkLeft = new EditablePose(-30, -10, toRadians(0));
 }
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,64 +1,129 @@
 package com.example.meepmeeptesting;
 
+import static com.example.meepmeeptesting.AutonVars.EXTEND_SAMPLE_1;
+import static com.example.meepmeeptesting.AutonVars.LENGTH_ROBOT;
+import static com.example.meepmeeptesting.AutonVars.WAIT_APPROACH_BASKET;
+import static com.example.meepmeeptesting.AutonVars.WAIT_POST_INTAKING;
+import static com.example.meepmeeptesting.AutonVars.WAIT_SCORE_BASKET;
+import static com.example.meepmeeptesting.AutonVars.WIDTH_ROBOT;
 import static com.example.meepmeeptesting.AutonVars.X_START_LEFT;
 import static com.example.meepmeeptesting.AutonVars.X_START_RIGHT;
 import static com.example.meepmeeptesting.AutonVars.Y_START;
-
+import static com.example.meepmeeptesting.AutonVars.basket;
+import static com.example.meepmeeptesting.AutonVars.sample1;
+import static com.example.meepmeeptesting.AutonVars.sample1Floor;
 import static java.lang.Math.PI;
+import static java.lang.Math.atan2;
 
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+
+    private static Action asyncIntakeSequence(double extension) {
+        return new SequentialAction(
+            new InstantAction(() -> {
+                // robot.intake.extendo.setTarget(extension);
+                // robot.intake.runRoller(0.8);
+            }),
+            telemetryPacket -> {
+                // return !robot.intake.hasSample();
+                return false;
+            },
+            new SleepAction(WAIT_POST_INTAKING),
+            new InstantAction(() -> {
+                // robot.intake.runRoller(0);
+            })
+        );
+    }
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 15)
-                .build();
-
         boolean isRight = false;
 
-        Pose2d startPose = new Pose2d(isRight ? X_START_RIGHT : X_START_LEFT, Y_START, Math.toRadians(0));
+        Pose2d startPose = new Pose2d(isRight ? X_START_RIGHT : X_START_LEFT, Y_START, isRight ? 0.5 * PI : 0);
 
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, 5.3, 20.378758517977975, 14.47)
+                .setDimensions(WIDTH_ROBOT, LENGTH_ROBOT)
+                .setStartPose(startPose)
+                .build();
+
+        sample1.heading = atan2(sample1Floor.y - sample1.y, sample1Floor.x - sample1.x);
+
+        Action scoreSample = new SequentialAction(
+                new SleepAction(WAIT_APPROACH_BASKET),
+                telemetryPacket -> {
+                    // return !robot.deposit.reachedTarget();
+                    return false;
+                },
+                new InstantAction(() -> {
+                    // robot.deposit.triggerClaw();
+                }),
+                new SleepAction(WAIT_SCORE_BASKET)
+        );
+
+        Action waitForIntake = telemetryPacket -> {
+            // return robot.getSample() == null;
+            return false;
+        };
+
+        Action raiseLift = new SequentialAction(
+                telemetryPacket -> {
+                    // return !robot.deposit.hasSample();
+                    return false;
+                },
+                new InstantAction(() -> {
+                    // robot.deposit.setPosition(HIGH);
+                })
+        );
 
         TrajectoryActionBuilder builder = myBot.getDrive().actionBuilder(startPose)
-                .setTangent(Math.toRadians(150))
 
-                // Score preloaded
-                .splineToLinearHeading(new Pose2d(-52, -53, Math.toRadians(45)), Math.toRadians(135))
-                .waitSeconds(0.1)
+                /// Score preloaded
+                .afterTime(0, raiseLift)
+                .strafeToSplineHeading(basket.toVector2d(), basket.heading)
+                .stopAndAdd(scoreSample)
 
-                // Grab first
-                .splineTo(new Vector2d(-48, -45), Math.toRadians(90))
-                .waitSeconds(0.2)
+                /// Intake first
+                .afterTime(0, asyncIntakeSequence(EXTEND_SAMPLE_1))
+                .strafeToSplineHeading(sample1.toVector2d(), sample1.heading)
+                .afterTime(0, () -> {
+                    // if (!robot.intake.hasSample()) robot.intake.runRoller(1);
+                })
+                .stopAndAdd(waitForIntake) // wait for sample
+                .waitSeconds(5) // simulate intaking
 
-                // Score first
-                .setReversed(true)
-                .splineTo(new Vector2d(-52, -53), Math.toRadians(225))
-                .waitSeconds(0.1)
+                /// Score first
+                .afterTime(0, raiseLift)
+                .strafeToSplineHeading(basket.toVector2d(), basket.heading)
+                .stopAndAdd(scoreSample)
 
-                // Grab second
-                .setReversed(false)
-                .turnTo(Math.toRadians(105))
-//                .splineTo(new Vector2d(-54, -45), Math.toRadians(90))
-                .waitSeconds(10)
+//                // Score first
+//                .setReversed(true)
+//                .splineTo(new Vector2d(-52, -53), toRadians(225))
+//                .waitSeconds(0.1)
 
-                // Score second
-                .setReversed(true)
-                .splineTo(new Vector2d(-52, -53), Math.toRadians(225))
-                .waitSeconds(10)
-
-                // Something else
-                .setReversed(false)
-                .splineToConstantHeading(new Vector2d(0, -55), Math.toRadians(90))
-                .waitSeconds(0.2);
+//                // Grab second
+//                .setReversed(false)
+//                .turnTo(toRadians(105))
+////                .splineTo(new Vector2d(-54, -45), Math.toRadians(90))
+//                .waitSeconds(10)
+//
+//                // Score second
+//                .setReversed(true)
+//                .splineTo(new Vector2d(-52, -53), toRadians(225))
+////                .waitSeconds(10)
+                ;
 
 
 
