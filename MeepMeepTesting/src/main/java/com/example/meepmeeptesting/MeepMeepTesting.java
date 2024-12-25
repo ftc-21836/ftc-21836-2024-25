432f2738073185ca5e6d6a40a5e4017e3478c866
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-24 19:03:54 -0500

    tested some auto paths
    Co-Authored-By: Aman Anas <41600372+Aman-Anas@users.noreply.github.com>
    Co-Authored-By: Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,7 +6,10 @@ import static com.example.meepmeeptesting.AutonVars.Y_START;
 
 import static java.lang.Math.PI;
 
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -17,15 +20,49 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(40, 40, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         boolean isRight = false;
 
-        Pose2d startPose = new Pose2d(isRight ? X_START_RIGHT : X_START_LEFT, Y_START, 0.5 * PI);
-        myBot.runAction(myBot.getDrive().actionBuilder(startPose)
-                .lineToY(-30)
-                .build());
+        Pose2d startPose = new Pose2d(isRight ? X_START_RIGHT : X_START_LEFT, Y_START, Math.toRadians(0));
+
+
+        TrajectoryActionBuilder builder = myBot.getDrive().actionBuilder(startPose)
+                .setTangent(Math.toRadians(150))
+
+                // Score preloaded
+                .splineToLinearHeading(new Pose2d(-52, -53, Math.toRadians(45)), Math.toRadians(135))
+                .waitSeconds(0.1)
+
+                // Grab first
+                .splineTo(new Vector2d(-48, -45), Math.toRadians(90))
+                .waitSeconds(0.2)
+
+                // Score first
+                .setReversed(true)
+                .splineTo(new Vector2d(-52, -53), Math.toRadians(225))
+                .waitSeconds(0.1)
+
+                // Grab second
+                .setReversed(false)
+                .turnTo(Math.toRadians(105))
+//                .splineTo(new Vector2d(-54, -45), Math.toRadians(90))
+                .waitSeconds(10)
+
+                // Score second
+                .setReversed(true)
+                .splineTo(new Vector2d(-52, -53), Math.toRadians(225))
+                .waitSeconds(10)
+
+                // Something else
+                .setReversed(false)
+                .splineToConstantHeading(new Vector2d(0, -55), Math.toRadians(90))
+                .waitSeconds(0.2);
+
+
+
+        myBot.runAction(builder.build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
