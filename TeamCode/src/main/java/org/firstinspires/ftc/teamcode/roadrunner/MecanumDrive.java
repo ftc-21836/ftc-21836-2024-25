baed6b8f67e2ec888887409a6e955cc9080b181f
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-30 15:19:28 -0500

    sus calculated inPerTick 😊
    Co-Authored-By: Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -166,7 +166,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
 
         // drive model parameters
-        public double inPerTick = 0.0005737329718469917;
+        public double inPerTick = PinpointLocalizer.INCH_PER_MM / PinpointLocalizer.TICKS_PER_MM;
         public double lateralInPerTick = inPerTick;
         public double trackWidthTicks = 0;
 
