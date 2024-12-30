3833586940731e66dc691cb3060721870d118cf8
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-29 20:50:43 -0500

    thresholding sample colors (sub pipeline)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/vision/pipeline/SubmersiblePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/vision/pipeline/SubmersiblePipeline.java
@@ -1,45 +1,16 @@
 package org.firstinspires.ftc.teamcode.control.vision.pipeline;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class SubmersiblePipeline extends OpenCvPipeline {
 
     private final Telemetry telemetry;
 
-    private final Scalar
-            minRed = new Scalar(
-                    0,
-                    0.25,
-                    0
-            ),
-            maxRed = new Scalar(
-                    30,
-                    0.75,
-                    0.06
-            ),
-            minYellow = new Scalar(
-                    80,
-                    0.6,
-                    0
-            ),
-            maxYellow = new Scalar(
-                    96,
-                    0.85,
-                    0.3
-            ),
-            minBlue = new Scalar(
-                    215,
-                    0.6,
-                    0
-            ),
-            maxBlue = new Scalar(
-                    230,
-                    0.9,
-                    0.1
-            );
+    public Sample targetColor = Sample.RED;
 
     public SubmersiblePipeline(Telemetry t) {
         telemetry = t;
@@ -49,6 +20,11 @@ public class SubmersiblePipeline extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         // Executed every time a new frame is dispatched
 
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2HSV);
+
+        Core.inRange(input, targetColor.min, targetColor.max, input);
+
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_GRAY2RGB);
         return input;
     }
 }
