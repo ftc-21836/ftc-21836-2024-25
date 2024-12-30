298e69d02b63257618b0300cac995bcbc87f26d4
Arshad Anas <arshadanas900+github@gmail.com>
2024-12-30 16:27:53 -0500

    pass pinpoint encoders to rr tuning opmodes
    Co-Authored-By: Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/TuningOpModes.java
@@ -16,11 +16,19 @@ import com.acmerobotics.roadrunner.ftc.LateralPushTest;
 import com.acmerobotics.roadrunner.ftc.LateralRampLogger;
 import com.acmerobotics.roadrunner.ftc.ManualFeedforwardTuner;
 import com.acmerobotics.roadrunner.ftc.MecanumMotorDirectionDebugger;
+import com.acmerobotics.roadrunner.ftc.RawEncoder;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeMeta;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.TankDrive;
@@ -77,6 +85,113 @@ public final class TuningOpModes {
 //                } else {
 //                    throw new RuntimeException("unknown localizer: " + md.localizer.getClass().getName());
 //                }
+                RawEncoder forward = new RawEncoder(new DcMotorEx() {
+                    @Override public void setMotorEnable() {
+                }
+                @Override public void setMotorDisable() {
+                }
+                @Override public boolean isMotorEnabled() {return false;}
+                    @Override public void setVelocity(double angularRate) {
+                }
+                @Override public void setVelocity(double angularRate, AngleUnit unit) {
+                }
+                @Override public double getVelocity() {return 0;}
+                    @Override public double getVelocity(AngleUnit unit) {return 0;}
+                    @Override public void setPIDCoefficients(RunMode mode, PIDCoefficients pidCoefficients) {
+                }
+                @Override public void setPIDFCoefficients(RunMode mode, PIDFCoefficients pidfCoefficients) throws UnsupportedOperationException {
+                }
+                @Override public void setVelocityPIDFCoefficients(double p, double i, double d, double f) {
+                }
+                @Override public void setPositionPIDFCoefficients(double p) {
+                }
+                @Override public PIDCoefficients getPIDCoefficients(RunMode mode) {return null;}
+                    @Override public PIDFCoefficients getPIDFCoefficients(RunMode mode) {return null;}
+                    @Override public void setTargetPositionTolerance(int tolerance) {
+                }
+                @Override public int getTargetPositionTolerance() {return 0;}
+                    @Override public double getCurrent(CurrentUnit unit) {return 0;}
+                    @Override public double getCurrentAlert(CurrentUnit unit) {return 0;}
+                    @Override public void setCurrentAlert(double current, CurrentUnit unit) {
+                }
+                @Override public boolean isOverCurrent() {return false;}
+                    @Override public MotorConfigurationType getMotorType() {return null;}
+                    @Override public void setMotorType(MotorConfigurationType motorType) {
+                }
+                @Override public DcMotorController getController() {return null;}
+                    @Override public int getPortNumber() {return 0;}
+                    @Override public void setZeroPowerBehavior(ZeroPowerBehavior zeroPowerBehavior) {
+                }
+                @Override public ZeroPowerBehavior getZeroPowerBehavior() {return null;}
+                    @Override public void setPowerFloat() {
+                }
+                @Override public boolean getPowerFloat() {return false;}
+                    @Override public void setTargetPosition(int position) {
+                }
+                @Override public int getTargetPosition() {return 0;}
+                    @Override public boolean isBusy() {return false;}
+
+                    @Override
+                    public int getCurrentPosition() {
+                        return md.localizer.rawEncoderX();
+                    }
+                    @Override public void setMode(RunMode mode) {}@Override public RunMode getMode() {return null;}@Override public void setDirection(Direction direction) {}@Override public Direction getDirection() {return null;}@Override public void setPower(double power) {}@Override public double getPower() {return 0;}@Override public Manufacturer getManufacturer() {return null;}@Override public String getDeviceName() {return "";}@Override public String getConnectionInfo() {return "";}@Override public int getVersion() {return 0;}@Override public void resetDeviceConfigurationForOpMode() {}@Override public void close() {}
+                });
+                RawEncoder perp = new RawEncoder(new DcMotorEx() {
+                    @Override public void setMotorEnable() {
+                    }
+                    @Override public void setMotorDisable() {
+                    }
+                    @Override public boolean isMotorEnabled() {return false;}
+                    @Override public void setVelocity(double angularRate) {
+                    }
+                    @Override public void setVelocity(double angularRate, AngleUnit unit) {
+                    }
+                    @Override public double getVelocity() {return 0;}
+                    @Override public double getVelocity(AngleUnit unit) {return 0;}
+                    @Override public void setPIDCoefficients(RunMode mode, PIDCoefficients pidCoefficients) {
+                    }
+                    @Override public void setPIDFCoefficients(RunMode mode, PIDFCoefficients pidfCoefficients) throws UnsupportedOperationException {
+                    }
+                    @Override public void setVelocityPIDFCoefficients(double p, double i, double d, double f) {
+                    }
+                    @Override public void setPositionPIDFCoefficients(double p) {
+                    }
+                    @Override public PIDCoefficients getPIDCoefficients(RunMode mode) {return null;}
+                    @Override public PIDFCoefficients getPIDFCoefficients(RunMode mode) {return null;}
+                    @Override public void setTargetPositionTolerance(int tolerance) {
+                    }
+                    @Override public int getTargetPositionTolerance() {return 0;}
+                    @Override public double getCurrent(CurrentUnit unit) {return 0;}
+                    @Override public double getCurrentAlert(CurrentUnit unit) {return 0;}
+                    @Override public void setCurrentAlert(double current, CurrentUnit unit) {
+                    }
+                    @Override public boolean isOverCurrent() {return false;}
+                    @Override public MotorConfigurationType getMotorType() {return null;}
+                    @Override public void setMotorType(MotorConfigurationType motorType) {
+                    }
+                    @Override public DcMotorController getController() {return null;}
+                    @Override public int getPortNumber() {return 0;}
+                    @Override public void setZeroPowerBehavior(ZeroPowerBehavior zeroPowerBehavior) {
+                    }
+                    @Override public ZeroPowerBehavior getZeroPowerBehavior() {return null;}
+                    @Override public void setPowerFloat() {
+                    }
+                    @Override public boolean getPowerFloat() {return false;}
+                    @Override public void setTargetPosition(int position) {
+                    }
+                    @Override public int getTargetPosition() {return 0;}
+                    @Override public boolean isBusy() {return false;}
+
+                    @Override
+                    public int getCurrentPosition() {
+                        return md.localizer.rawEncoderY();
+                    }
+                    @Override public void setMode(RunMode mode) {}@Override public RunMode getMode() {return null;}@Override public void setDirection(Direction direction) {}@Override public Direction getDirection() {return null;}@Override public void setPower(double power) {}@Override public double getPower() {return 0;}@Override public Manufacturer getManufacturer() {return null;}@Override public String getDeviceName() {return "";}@Override public String getConnectionInfo() {return "";}@Override public int getVersion() {return 0;}@Override public void resetDeviceConfigurationForOpMode() {}@Override public void close() {}
+                });
+
+                parEncs.add(forward);
+                perpEncs.add(perp);
 
                 return new DriveView(
                     DriveType.MECANUM,
