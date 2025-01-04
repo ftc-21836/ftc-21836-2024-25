5c7fbf79f533347a509ed43efa2b7f38e6ee3325
Arshad Anas <arshadanas900+github@gmail.com>
2025-01-04 16:44:53 -0500

    tuning sample side auton (intaking 3 pose)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainAuton.java
@@ -303,7 +303,7 @@ public final class MainAuton extends LinearOpMode {
                         .afterTime(0, () -> robot.intake.runRoller(SPEED_INTAKING))
                         .strafeToSplineHeading(intakingPos.toVector2d(), intakingPos.heading)
                         .afterTime(0, new SequentialAction(
-                                telemetryPacket -> !robot.intake.hasSample() && robot.intake.extendo.getPosition() < millimeters - 5,
+                                telemetryPacket -> !robot.intake.hasSample() && robot.intake.extendo.getPosition() < millimeters - 10,
                                 new InstantAction(() -> {
                                     if (!robot.intake.hasSample()) robot.intake.runRoller(1);
                                 })
