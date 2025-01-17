ab9c629f9e4402e06271317fa542766cdb21ed46
Yousuf Siddiqui <yousufsiddiqui1707@gmail.com>
2025-01-16 20:01:19 -0500

    fix arm moving to intake pos afte rscoring spec
    Co-Authored-By: Arshad Anas <79271473+arshadanas@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/MainAuton.java
@@ -68,7 +68,7 @@ public final class MainAuton extends LinearOpMode {
             WAIT_APPROACH_CHAMBER = 0,
             WAIT_POST_INTAKING = 0.5,
             WAIT_SCORE_BASKET = 0.25,
-            WAIT_SCORE_CHAMBER = 0.5,
+            WAIT_SCORE_CHAMBER = 0.75,
             WAIT_DROP_TO_EXTEND = 0.75,
             X_OFFSET_CHAMBER_1 = 1,
             X_OFFSET_CHAMBER_2 = -1,
