4eea1bd68b91e3868c63907b6a3821b2d9c70c13
Yousuf Siddiqui <yousufsiddiqui1707@gmail.com>
2025-01-26 16:19:01 -0500

    decrease max angular acceleration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -177,7 +177,7 @@ public class MecanumDrive {
 
         // turn profile parameters (in radians)
         public double maxAngVel = 5.3; // shared with path
-        public double maxAngAccel = 20;
+        public double maxAngAccel = 10;
 
         // path controller gains
         public double axialGain = 8;
