e9d2b29592cc5ce89c9e065f6b9e73bcccbf2a91
Arshad Anas <arshadanas900+github@gmail.com>
2025-02-06 17:28:45 -0500

    adjust intaking2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/Auto.java
@@ -112,12 +112,12 @@ public final class Auto extends LinearOpMode {
 
             sample1SpecPreload = new EditablePose(-50, -27.75, PI / 2),
             sample1 = new EditablePose(-49.24,-28.3, PI / 2),
-            sample2 = new EditablePose(-58, -27.75, PI / 2),
+            sample2 = new EditablePose(-59.3,-29, PI / 2),
             sample3 = new EditablePose(-68,-22, PI / 2),
     
             intaking1SpecPreload = new EditablePose(-51, -46, toRadians(84.36)),
             intaking1 = new EditablePose(-54,-48, toRadians(84.36)),
-            intaking2 = new EditablePose(-53.02, -49.21, toRadians(105)),
+            intaking2 = new EditablePose(-56.7,-48.04, toRadians(105)),
             intaking3 = new EditablePose(-54, -43, 2 * PI / 3),
     
             basket = new EditablePose(-55.5, -55.5, PI / 4),
