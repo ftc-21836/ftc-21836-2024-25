5dc0e99f07af65a7401571db5710d247ceeabde1
Arshad Anas <arshadanas900+github@gmail.com>
2025-02-07 21:03:17 -0500

    minor inc ang vel, dec ang accel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -180,8 +180,8 @@ public class MecanumDrive {
         public double maxProfileAccel = 60;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = PI; // shared with path
-        public double maxAngAccel = 10;
+        public double maxAngVel = 3.5; // shared with path
+        public double maxAngAccel = 7.5;
 
         // path controller gains
         public double axialGain = 8;
