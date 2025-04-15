59b3d261656bf8f589fb6367bf206d72a3fa0ced
Yousuf Siddiqui <97410861+xzele@users.noreply.github.com>
2025-04-14 22:04:35 -0400

    Update SubmersiblePipeline.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/vision/pipeline/SubmersiblePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/vision/pipeline/SubmersiblePipeline.java
@@ -33,8 +33,8 @@ public class SubmersiblePipeline extends OpenCvPipeline {
             fractionOfByYs = 0.5,
             fractionOfDensities = 0.5,
 
-            SCREEN_WIDTH = 800,
-            SCREEN_HEIGHT = 600;
+            SCREEN_WIDTH = 1280,
+            SCREEN_HEIGHT = 960;
 
     private final Point cornerBR = new Point(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -53,8 +53,8 @@ public class SubmersiblePipeline extends OpenCvPipeline {
                     maxRed = new Scalar(255, 255.0, 255.0),
                     minBlue = new Scalar(55, 145, 0),
                     maxBlue = new Scalar(145, 255.0, 255.0),
-                    minYellow = new Scalar(0, 36.8, 0),
-                    maxYellow = new Scalar(60.9, 255.0, 255.0);
+                    minYellow = new Scalar(11, 83, 60),
+                    maxYellow = new Scalar(52.4, 255.0, 255.0);
 
     public int lineThickness = 6;
 
@@ -103,32 +103,33 @@ public class SubmersiblePipeline extends OpenCvPipeline {
             return area < minSize || area > maxSize;
         });
 
-        // contour area
-        {
-            contours.sort((c1, c2) -> (int) (100000000 * (contourArea(c2) - contourArea(c1))));
-            int numContours = (int) round(contours.size() * fractionOfContours);
-            while (contours.size() > numContours) contours.remove(contours.size() - 1);
-        }
-
-        // density
-        {
-            contours.sort((c1, c2) -> {
-                contours2f.release();
-                c2.convertTo(contours2f, CvType.CV_32F);
-                RotatedRect r2 = Imgproc.minAreaRect(contours2f);
-
-                contours2f.release();
-                c1.convertTo(contours2f, CvType.CV_32F);
-                RotatedRect r1 = Imgproc.minAreaRect(contours2f);
-
-                return (int) (100000000 * (
-                        contourArea(c2) / r2.size.area() -
-                                contourArea(c1) / r1.size.area()
-                ));
-            });
-            int numContoursDensity = (int) round(contours.size() * fractionOfDensities);
-            while (contours.size() > numContoursDensity) contours.remove(contours.size() - 1);
-        }
+//        // contour area
+//        {
+//            contours.sort((c1, c2) -> (int) (100000000 * (contourArea(c2) - contourArea(c1))));
+//            int numContours = (int) round(contours.size() * fractionOfContours);
+//            while (contours.size() > numContours) contours.remove(contours.size() - 1);
+//        }
+//
+//
+//        // density
+//        {
+//            contours.sort((c1, c2) -> {
+//                contours2f.release();
+//                c2.convertTo(contours2f, CvType.CV_32F);
+//                RotatedRect r2 = Imgproc.minAreaRect(contours2f);
+//
+//                contours2f.release();
+//                c1.convertTo(contours2f, CvType.CV_32F);
+//                RotatedRect r1 = Imgproc.minAreaRect(contours2f);
+//
+//                return (int) (100000000 * (
+//                        contourArea(c2) / r2.size.area() -
+//                                contourArea(c1) / r1.size.area()
+//                ));
+//            });
+//            int numContoursDensity = (int) round(contours.size() * fractionOfDensities);
+//            while (contours.size() > numContoursDensity) contours.remove(contours.size() - 1);
+//        }
 
         // closest to bottom right
         {
@@ -148,7 +149,7 @@ public class SubmersiblePipeline extends OpenCvPipeline {
                 return (int) (1000000 * (d1 - d2));
             });
             int numContoursByY = (int) round(contours.size() * fractionOfByYs);
-            while (contours.size() > 1) contours.remove(contours.size() - 1);
+            while (contours.size() > numContoursByY) contours.remove(contours.size() - 1);
         }
 
         int i = 0;
@@ -175,17 +176,19 @@ public class SubmersiblePipeline extends OpenCvPipeline {
         }
 
 
-        contours2f.release();
-        contours.get(0).convertTo(contours2f, CvType.CV_32F);
+        if (!contours.isEmpty()) {
+            contours2f.release();
+            contours.get(0).convertTo(contours2f, CvType.CV_32F);
 
-        Point center = Imgproc.minAreaRect(contours2f).center;
-        target.x = center.x;
-        target.y = center.y;
+            Point center = Imgproc.minAreaRect(contours2f).center;
+            target.x = center.x;
+            target.y = center.y;
 
-        telemetry.addData("Target X", target.x);
-        telemetry.addData("Target Y", target.y);
-        telemetry.update();
-        Imgproc.drawMarker(input, target, green, 2, 8);
+            telemetry.addData("Target X", target.x);
+            telemetry.addData("Target Y", target.y);
+            telemetry.update();
+            Imgproc.drawMarker(input, target, green, 2, 8);
+        }
 
         if (viewContours) return hsvBinaryMat;
         hsvBinaryMat.release();
